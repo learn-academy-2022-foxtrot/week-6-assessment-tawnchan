c3c80522ea14b4e1d9a0566892cfe0b6b2fd1a93
tawnchan <tawnchan@gmail.com>
2022-10-25 17:30:10 -0700

    completed all all assignments

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -16,31 +16,134 @@
 
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+  { name: "zaphod beeblebrefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+describe("about", () => {
+  it(
+    "returns an array with a sentence about each person with their name capitalized",
+    () => {
+      expect(about(people)).toEqual([
+        "Ford Prefect is a hitchhiker.",
+        "Zaphod Beeblebrox is president of the galaxy.",
+        "Arthur Dent is a radio employee."
+      ]);
+    });
+});
+
+
+// ReferenceError: about is not defined, This is a good error.... now we just have to build out the function to make this test pass.
 
 // b) Create the function that makes the test pass.
+
+// Since the first character of each element of the object array needs to be capitalized.. and theNNNn...
+// We need to iterate through each element and pull the first character of the name key and capitalize it... and theNNNn
+// We can do this by using .map() to iterate through each element and push the name key into a new array.
+// thenNn.. I need to figure out two split up the keys from their values.. and theNnn.. there is the .split()
+// what we'll do is once they're split we will map through the elements in the new array, 
+// access charAt(0) attached .toUpperCase and since it capitalized all the characters, we will use .slice(1)
+// on each element to separate the at index 1 of the element so isolate the first capitalized character
+// we then use .join() to join back the name element
+// finally we string interpolation to form the sentence using the key value pairs: name and occupation
+// and return the new array of sentences. 
+
+const about = (people) => {
+  let sentence = people.map((person) => {
+    const namesArray = person.name.split(" ");
+    const names = namesArray
+      .map((element) => {
+        return (element = element.charAt(0).toUpperCase() + element.slice(1));
+      })
+      .join(" ");
+    return `${names} is ${person.occupation}.`;
+  });
+  return sentence;
+};
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.157 s
+// Ran all test suites.
+// ✨  Done in 0.55s.
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false];
 // Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true];
 // Expected output: [ 2, 1, -1 ]
+
+describe("remainingNumbers", () => {
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+    expect(remainingNumbers(hodgepodge1)).toEqual([2, 0, -1, 0]);
+    expect(remainingNumbers(hodgepodge2)).toEqual([2, 1, -1]);
+  });
+});
+
+// ReferenceError: remainingNumbers is not defined, good fail, lets make a function to make this test passsssssss....
 
 // b) Create the function that makes the test pass.
 
-// --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
+// b) Create the function that makes the test pass.
+// we need a function that takes in an array and checks for division by three
+// first thing we need to do is filter out all of the non-number elements. We can use filter method
+// once we have the arrays with just numbers, we can chain .map method and divide each number using mod operator
+// resulting array will have division by 3 remainders as elements
 
-// a) Create a test with an expect statement using the variables provided.
+// First thing is first.. we have to figure out how to select only the numbers of the mixed data array.
+// We'll use filter() on the elements and select using type() we will select the elements that are strictly
+// equal to 'number' followed by a map(elements) to return the new array of numbers divided by 3.
+
+const remainingNumbers = (array) =>
+  array.filter((elements) => 
+  typeof elements === 'number')
+  .map((elements) => elements % 3);
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.19 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.97s.
+
+
+// // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
+
+// // a) Create a test with an expect statement using the variables provided.
+
+
+// The instructions ar e confusing... the output is 99.. but the instructions asked for the SUM of ALL the numbers... cubed. This means that you have to add the numbers in teh array first and then cube it... you know what i mean? so the output shouldn't 99 or 
 
 const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe("cubed", () => {
+  it("takes in an array of numbers and returns the sum of all the numbers cubed.", () => {
+    expect(cubed(cubeAndSum1)).toEqual(99);
+    expect(cubed(cubeAndSum2)).toEqual(1125);
+  });
+});
+
+// ReferenceError: cubed is not defined... lets define it theNNnNn...
+
 // b) Create the function that makes the test pass.
+
+// Since we have an array of numbers where we have to do something to each element,
+// can iterate through each element of the array using .map() and cubing each element,
+// which is element to the third power... we use **.
+// After cubing each element, we have to then add the new total of each element
+// which was created with the .map() function.
+// We can use .reduce() to add the totals of the element in the array. 
+
+const cubed = (array) => array.map((element) => element ** 3).reduce((b, a) => b + a);
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.192 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.68s.
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -7,56 +7,100 @@
 # FILE: app/controller/blog_posts_controller.rb
 
 # ---1)
-class BlogPostsController < ApplicationController
-  def index
-    # ---2)
-    @posts = BlogPost.all
-  end
 
+# --- 
+  # 1) There is a class of BlogPostController that is a child of the ApplicationController.
+  # 2) This class has a method called 'index' 
+
+  class BlogPostsController < ApplicationController
+    def index
+  
+  # ---2)
+  
+      # 1) @post is tied to requesting all the index data in the BlogPost database.
+      # 2) It allows us to view the index.
+  
+      @posts = BlogPost.all
+    end
+  
   # ---3)
-  def show
-    @post = BlogPost.find(params[:id])
-  end
-
+  
+      # 1) There is a method called 'show'
+      # 2) it takes in the parameter of the :id key.
+      # 3) So this is a method that finds a specific blog post.
+  
+    def show
+      @post = BlogPost.find(params[:id])
+    end
+  
   # ---4)
-  def new
-    @post = BlogPost.new
-  end
-
-  def create
-    # ---5)
+  
+      # 1) There is a method called 'new'
+      # 2) This one posts a new blog post.
+  
+    def new
+      @post = BlogPost.new
+    end
+  
+      # 3) The one below this is a method called 'create'
+    def create
+      
+  # ---5)
+      
+      # 4) It creates new parameters for the database BlogPost.
+  
     @post = BlogPost.create(blog_post_params)
-    if @post.valid?
-      redirect_to blog_post_path(@post)
+      if @post.valid?
+        redirect_to blog_post_path(@post)
+      end
     end
-  end
-
-  def edit
-    # ---6)
-    @post = BlogPost.find(params[:id])
-  end
-
-  def update
-    @post = BlogPost.find(params[:id])
-    # ---7)
-    @post.update(blog_post_params)
-    if @post.valid?
-      redirect_to blog_post_path(@post)
+  
+    def edit
+  
+  # ---6)
+  
+      # 1) The method 'edit' finds different blog post by :id key.
+  
+      @post = BlogPost.find(params[:id])
     end
-  end
-
-  def destroy
-    @post = BlogPost.find(params[:id])
-    if @post.destroy
-      # ---8)
-      redirect_to blog_posts_path
+  
+    def update
+      @post = BlogPost.find(params[:id])
+  
+  # ---7)
+  
+      # 1) The method 'update' edits/updates the blog posts if there is one.
+  
+      @post.update(blog_post_params)
+      if @post.valid?
+        redirect_to blog_post_path(@post)
+      end
     end
-  end
-
+  
+    def destroy
+      @post = BlogPost.find(params[:id])
+      if @post.destroy
+        
+  # ---8)
+  
+      # 1) the method 'destroy' is used to destroy/delete a specific blog post by its id.
+  
+        redirect_to blog_posts_path
+      end
+    end
+  
   # ---9)
-  private
-  def blog_post_params
-    # ---10)
-    params.require(:blog_post).permit(:title, :content)
+  
+    # 1) This private method below is used to hid something. i have no idea.
+  
+    private
+    def blog_post_params
+  
+  # ---10)
+  
+      # 1) The method 'blog_post_params' is used to require specific parameters, key/value pairs when new instances are created by this class.
+  
+      params.require(:blog_post).permit(:title, :content)
+    end
   end
-end
+  
